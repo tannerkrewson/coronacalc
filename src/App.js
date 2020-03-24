fba680c56a84456ad4e3e773887b87dae9690585
Tanner Krewson <tannerkrewson@gmail.com>
2020-03-24 12:36:56 -0500

    remove react default and add title

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h2>
+          US Coronavirus Check Calculator
+        </h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The average American will receive $1500. How much will you get?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
