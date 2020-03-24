5b8fd21b811e5109f699eb5637ac35ee222e21f2
Tanner Krewson <tannerkrewson@gmail.com>
2020-03-24 14:38:43 -0500

    added prettier

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React from 'react';
-import Calculator from './components/Calculator'
-import './App.css';
+import React from "react";
+import Calculator from "./components/Calculator";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h2>
-          Check Calculator
-        </h2>
-        <p>
-          ----
-        </p>
-      </header>
-      <Calculator/>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h2>Check Calculator</h2>
+				<p>----</p>
+			</header>
+			<Calculator />
+		</div>
+	);
 }
 
 export default App;
